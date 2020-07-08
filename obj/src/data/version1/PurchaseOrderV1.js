a21a7e5bd1b9a981ab1dbd3c6d19739bcbcc1a42
Denis Kuznetsov <deniskuzn@gmail.com>
2020-07-08 17:47:51 +0300

    init commit

---
+++ obj/src/data/version1/PurchaseOrderV1.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class PurchaseOrderV1 {
+}
+exports.PurchaseOrderV1 = PurchaseOrderV1;
+//# sourceMappingURL=PurchaseOrderV1.js.map
